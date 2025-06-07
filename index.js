a2f532e65c0a6d8ae0555829d1ae80e62588ae37
laurenannmurugi <laurenann1871@gmail.com>
2025-06-07 17:29:13 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+//Declare a variable 
+//Use the var keyword
+//assign the value
+
+var customerName = "bob";
+// access the variable in the global scope
+function upperCaseCustomerName() {
+ customerName = customerName.toUpperCase();
+}
+
+//Write a function
+//declare a variable and assign it a value
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+//Write a function
+//declare a variable and assign it a value
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// Declare a constant variable and assign it a value
+const leastFavoriteCustomer = 'not bob';
+//Write a function that attempts to change that constant
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'maybe bob';
+}
